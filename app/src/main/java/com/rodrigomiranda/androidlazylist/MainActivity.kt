a33c137f15888f174ee584738fb1689dc9f7fbb6
Rodrigo Miranda <rodrigo.kreloco@gmail.com>
2025-02-07 23:27:03 -0600

    Add rows and columns

--- app/src/main/java/com/rodrigomiranda/androidlazylist/MainActivity.kt
+++ app/src/main/java/com/rodrigomiranda/androidlazylist/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.rodrigomiranda.androidlazylist.ui.theme.AndroidLazyListTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +26,26 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AndroidLazyListTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MyApp(modifier = Modifier.fillMaxSize())
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(modifier: Modifier = Modifier, names: List<String> = listOf("World", "Compose")) {
+    Scaffold(
+        modifier = modifier
+    ) { innerPadding ->
+        Column(
+            modifier = modifier
+                .padding(innerPadding)
+                .safeDrawingPadding()
+        ) {
+            names.forEach {
+                Greeting(
+                    name = it
+                )
             }
         }
     }
@@ -32,16 +53,32 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        Row(
+            modifier = Modifier.padding(24.dp)
+        ) {
+            Column(
+                modifier = Modifier.weight(1F),
+            ) {
+                Text(text = "Hello")
+                Text(text = "$name!")
+            }
+            ElevatedButton(
+                onClick = { }
+            ) {
+                Text("Show more")
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, name = "Greeting Preview", widthDp = 320)
 @Composable
 fun GreetingPreview() {
     AndroidLazyListTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
